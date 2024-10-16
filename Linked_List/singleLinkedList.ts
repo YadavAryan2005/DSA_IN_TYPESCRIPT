e4f9d8b0a8f3c8e60cb6bb80ae34f89c6a5a3b70
YadavAryan2005 <aryanyadav7822900241@gmail.com>
2024-10-16 17:08:16 +0530

    sigle Linked list implement add and display

---
+++ Linked_List/singleLinkedList.ts
@@ -0,0 +1,40 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SingleLinkedList {
+  head: ListNode;
+  constructor(val: number) {
+    let newNode = new ListNode(val);
+    this.head = newNode;
+    this.head.next = null;
+  }
+  addNode(val: number) {
+    const newNode = new ListNode(val);
+    let current = this.head;
+    while (current?.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      if (current.next === null) break;
+      current = current.next;
+    }
+  }
+}
+
+let list = new SingleLinkedList(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+list.addNode(5);
+list.display();
